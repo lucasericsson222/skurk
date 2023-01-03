56be86123e1118af9c3a5993d42b1822ec6c6188
lucasericsson <lucasericsson222@gmail.com>
2023-01-03 13:34:55 -0800

    Convert to Styled Components

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,48 +2,58 @@ import { useEffect, useState } from 'react';
 import WorldGrid from './WorldGrid';
 import { Main, updateUIData } from '../game/Main';
 import { Pixel } from '../game/Pixel';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
 
-
+const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: black;
+        color: white;
+        font-family: monospace;
+        width: 100%;
+        height: 100%;
+        padding: 0;
+    }
+`
 
 function App() {
-  let [worldData, setWorldData] = useState<Pixel[][][]>([]);
-  let [tick, setTick] = useState<number>(0);
+    let [worldData, setWorldData] = useState<Pixel[][][]>([]);
+    let [tick, setTick] = useState<number>(0);
 
   
 
-  useEffect(() => {
+    useEffect(() => {
+        function updateUI(data: updateUIData) {
+            setWorldData(data.worldData);
+        }
 
-    function updateUI(data: updateUIData) {
-      setWorldData(data.worldData);
-    }
+        let handleInput = Main(updateUI);
 
+        function helper(e: globalThis.KeyboardEvent) {
+            handleInput(e);
+        }
 
-    let handleInput = Main(updateUI);
+        document.addEventListener("keydown", helper, false);
+        return () => {
+            document.removeEventListener("keydown", helper);
+        };
+    }, []);
 
-    function helper(e: globalThis.KeyboardEvent) {
-      handleInput(e);
-    }
-
-    document.addEventListener("keydown", helper, false);
-    return () => {
-      document.removeEventListener("keydown", helper);
-      
-    };
-  }, []);
-
-  useEffect(() => {
-    function incrementTick() {
-      setTick(tick + 1);
-    }
-    const timer = setInterval(incrementTick, 500);
-    return () => {
-      clearInterval(timer);
-    };
-  }, [tick]);
-  return (
-    <WorldGrid data={worldData} tick={tick}></WorldGrid>
-  );
+    useEffect(() => {
+        function incrementTick() {
+            setTick(tick + 1);
+        }
+        const timer = setInterval(incrementTick, 500);
+        return () => {
+            clearInterval(timer);
+        };
+    }, [tick]);
+    
+    return (
+        <>
+            <GlobalStyle/>
+            <WorldGrid data={worldData} tick={tick}></WorldGrid>
+        </>
+    );
 }
 
 export default App;
--- src/ui/WorldGrid.tsx
+++ src/ui/WorldGrid.tsx
@@ -1,63 +1,46 @@
-import "./WorldGrid.css";
 import { Pixel } from "../game/Pixel";
-import { Color } from "../game/Color";
 import PixelUI from "./PixelUI";
-import { relative } from "path";
+import styled from "styled-components";
 interface WorldGridProps {
     data: Pixel[][][];
     tick: number;
 }
+interface GridProps {
+    i: number;
+}
+
+const Grid = styled.div`
+    display:grid;
+    position: absolute;
+    gap: ${(props:GridProps) => props.i / 20}rem;
+    font-size: 3rem;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+
 
 function WorldGrid({data, tick}: WorldGridProps) {
-    //let numberOfIndex = 5;
-    //let numLines = 7;
-    //let numCharacters = 18;
-    let xTightness = 0.025;
-    //let halfLineExtraWidth = (numCharacters * 0.1)/2;
-    //let halfLineExtraHeight = (0.2 * numLines )/2; 
-    /*function brightness(index: number) {
-        return (index + 1) / (numberOfIndex + 1) * 255;
-    }*/
-    
     return (
-        <div className="world-grid"> 
+        <>
         {data.map((x: Pixel[][], index:number) => 
-            (<div 
-                key={index} 
-                id={"level" + index.toString()}
-                style={{
-                    display: "grid",
-                    position: "absolute",
-                    gap: `${index/ 20}rem`,
-                    fontSize: `3rem`,
-                    top: `50%`,
-                    left: `50%`,
-                    transform: `translate(-50%, -50%)`
-                    //letterSpacing: `${0.1 + xTightness*index}rem`,
-                    //paddingLeft: `${halfLineExtraWidth * (numberOfIndex - index) / numberOfIndex}rem`,
-                    //lineHeight: `${2.3 + 0.05 * index}rem`,
-                    //paddingTop: `${halfLineExtraHeight * (numberOfIndex - index) / numberOfIndex}rem`,
-                    //color: `rgb(${brightness(index)},${brightness(index)},${brightness(index)})`
-                }}
-            >
-                {
-                    x.map((pix, i) => {
-                        let j = 1;
-                        return pix.map((p, index) => {
-                            console.log(i)
-                            let hide = (index !== tick % pix.length);
-                            return <span style={{opacity: hide?0:'100%', gridColumn: i % 18 + 1, gridRow: Math.floor( i / 18 + 1) }}>
-                            {(p.symbol === Pixel.Empty.symbol)?
+            (<Grid i={index}>
+                {x.map((pix, i) => 
+                    pix.map((p, index) => {
+                        let hide = (index !== tick % pix.length);
+                        return (
+                            <span style={{opacity: hide?0:'100%', gridColumn: i % 18 + 1, gridRow: Math.floor( i / 18 + 1) }}>
+                            {/*if*/(p.symbol === Pixel.Empty.symbol)?
                                 /*then*/<>&nbsp;</>
                             /*elif*/:(p.color.length === 1)?
-                                <span style={{color:(()=> {return p.color.toRGB();})()}}>{p.symbol}</span>
+                                <span style={{color:(()=> {return p.color.toRGB();})()}}>{p.symbol}</span>                                    
                                 :<PixelUI symbol={p.symbol} color={p.color} tick={tick}></PixelUI>}
-                            </span>;
-                        })})
-                }
-            </div>)
-        )}
-        </div>
+                            </span>
+                        );
+                }))}
+            </Grid>
+        ))}
+        </>
     );
 }
 
