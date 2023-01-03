c2d3c8d66b3232166e93e2dfd40f07f4912eb6ad
lucasericsson <lucasericsson222@gmail.com>
2023-01-03 13:46:33 -0800

    Remove Line

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './ui/App';
-import reportWebVitals from './ui/reportWebVitals';
+import reportWebVitals from './ui/AppUtilities/reportWebVitals';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
